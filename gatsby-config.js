550e56ac3185351e5c0f3e83f357927d8289ead1
Bruno Godefroy <godefroy.bruno77@gmail.com>
2020-09-16 16:19:19 +0200

    Create a web manifest with a custom favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,17 @@ module.exports = {
   plugins: [
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Fimento',
+        short_name: 'Fimento',
+        start_url: '/',
+        background_color: '#dbc9be',
+        theme_color: '#dbc9be',
+        display: 'fullscreen',
+        icon: 'src/assets/favicon.png',
+      },
+    },
   ],
 };
