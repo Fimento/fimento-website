29d7262fcfc7cb257021b2f6caf956de72e6df36
Jacob Nyman <jacob.nyman@moank.com>
2020-10-06 17:04:43 +0200

    [4057] Add fimentoInfo

---
+++ src/constants/fimentoInfo.js
@@ -0,0 +1,9 @@
+const fimentoInfo = {
+  COMPANY_NAME: 'Fimento AB',
+  STREET: 'Jakobsbergsgatan 13 (8th floor)',
+  ADDRESS: '111 44 Stockholm, Sweden',
+  PHONE: '+46 76 858 37 64',
+  EMAIL: 'info@fimento.com',
+};
+
+export default fimentoInfo;
