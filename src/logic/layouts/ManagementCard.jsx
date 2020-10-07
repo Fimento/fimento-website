aec8d849b118a6194b320c2cf0fa80e145ea2aaa
Jacob Nyman <jacob.nyman@moank.com>
2020-10-07 17:31:35 +0200

    [4087] Update mangement card to be responsive

--- src/logic/layouts/ManagementCard.jsx
+++ src/logic/layouts/ManagementCard.jsx
@@ -1,18 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import StyledParagraph from '../../styled/elements/StyledParagraph';
 import StyledImg from '../../styled/elements/StyledImg';
-import StyledFlexBox from '../../styled/layouts/StyledFlexBox';
+
+import style from '../../constants/style';
+
+const StyledRole = styled(StyledParagraph)`
+  max-width: 58%;
+`;
+
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: ${style.colours.darkBeige};
+  margin: ${style.spaces.tinier};
+  width: calc(50% - (2 * ${style.spaces.tinier}));
+  min-height: 17rem;
+  max-height: 27rem;
+  ${StyledImg} {
+    width: 100%;
+  }
+  ${StyledParagraph} {
+    font-size: ${style.fontSizes.tiny};
+    margin: 0;
+  }
+
+  ${style.media.tablet`
+    flex-direction: row;
+    width: calc(50% - (2 * ${style.spaces.tiny}));
+    margin: ${style.spaces.tiny} ${style.spaces.small} ${style.spaces.tiny} 0;
+    height: auto;
+    ${StyledImg} {
+      width: 50%;
+      height: auto;
+    }
+    ${StyledRole} {
+      max-width: 100%;
+    }
+    ${StyledParagraph} {
+      font-size: ${style.fontSizes.small};
+      margin: ${style.spaces.tiny} 0 ${style.spaces.tiny} 0;
+    }
+  `}
+`;
+
+const StyledTextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: flex-start;
+  padding-left: ${style.spaces.small};
+  padding-top: ${style.spaces.big};
+  min-height: 4rem;
+
+  ${style.media.laptop`
+    flex-direction: column;
+    margin: ${style.spaces.tiny};
+    padding-top: ${style.spaces.huge};
+    height: 100%;
+  `}
+`;
 
 const ManagementCard = ({ img, name, title }) => (
-  <StyledFlexBox width="470px" height="225px" background="darkBeige" margin="small">
-    <StyledImg src={img} alt="" width="240px" height="225px" />
-    <StyledFlexBox width="100%" flexDirection="column" alignItems="start" justifyContent="center" paddingLeft="small" marginBottom="huge">
+  <StyledCard>
+    <StyledImg src={img} alt="" />
+    <StyledTextContent>
       <StyledParagraph textColour="white">{name}</StyledParagraph>
-      <StyledParagraph textColour="grey">{title}</StyledParagraph>
-    </StyledFlexBox>
-  </StyledFlexBox>
+      <StyledRole textColour="grey">{title}</StyledRole>
+    </StyledTextContent>
+  </StyledCard>
 );
 
 ManagementCard.propTypes = {
