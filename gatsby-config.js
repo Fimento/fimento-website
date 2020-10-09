9d005d70448d870e1c07f147f6449688e05d15ee
Jacob Nyman <jacob.nyman@moank.com>
2020-10-09 14:20:48 +0200

    [4089] Add plugins for blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,14 @@ module.exports = {
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: `${__dirname}/blog`,
+      },
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
