8d18cc3154521865e0a091b5f15112a4c4ce79c6
David Vergnault <david.vergnault@moank.com>
2020-10-16 15:16:42 +0200

    [4115] Move StyledArticle in its own component

---
+++ src/styled/layouts/StyledArticle.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+import style from '../../constants/style';
+
+import { DEFAULT_PARAGRAPH_FONT_SIZE } from '../elements/StyledParagraph';
+
+const StyledArticle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  width: 100%;
+
+  p {
+    width: 100%;
+    line-height: 1.5;
+    font-weight: ${style.fontWeights.light};
+    text-align: inherit;
+    font-style: normal;
+    font-size: ${DEFAULT_PARAGRAPH_FONT_SIZE};
+  }
+  strong {
+    width: 100%;
+    line-height: 1.5;
+    font-weight: ${style.fontWeights.bold};
+    text-align: inherit;
+    font-style: normal;
+    font-size: ${DEFAULT_PARAGRAPH_FONT_SIZE};
+  }
+  a {
+    color: ${style.colours.secondary};
+    text-decoration: none;
+    cursor: pointer;
+    background: none;
+    border: none;
+    padding: 0;
+    font: inherit;
+    outline: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  li {
+    margin: 0 0 ${style.spaces.tinier};
+    font-size: ${DEFAULT_PARAGRAPH_FONT_SIZE};
+    font-weight: ${style.fontWeights.light};
+    line-height: 1.5;
+  }
+
+  h2 {
+    font-size: 1.625rem;
+    font-weight: ${style.fontWeights.bold};
+    margin: ${style.spaces.regular} 0;
+  }
+
+  h3 {
+    font-size: ${style.fontSizes.regular};
+    font-weight: ${style.fontWeights.bold};
+    margin: ${style.spaces.big} 0 ${style.spaces.tiny};
+  }
+
+  p > img {
+    margin: auto;
+    display: block;
+  }
+`;
+
+export default StyledArticle;
--- src/templates/BlogPostTemplate.jsx
+++ src/templates/BlogPostTemplate.jsx
@@ -9,8 +9,9 @@ import Page from '../layout/Page';
 
 import Section from '../logic/layouts/Section';
 
+import StyledArticle from '../styled/layouts/StyledArticle';
+
 import StyledButton from '../styled/elements/StyledButton';
-import { DEFAULT_PARAGRAPH_FONT_SIZE } from '../styled/elements/StyledParagraph';
 import StyledH1 from '../styled/elements/StyledH1';
 import StyledH4 from '../styled/elements/StyledH4';
 
@@ -40,68 +41,6 @@ const StyledCoverImageSection = styled.section`
   background-repeat: no-repeat;
   height: 50vh;
   width: 100%;
-`;
-
-const StyledArticle = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  width: 100%;
-
-  p {
-    width: 100%;
-    line-height: 1.5;
-    font-weight: ${style.fontWeights.light};
-    text-align: inherit;
-    font-style: normal;
-    font-size: ${DEFAULT_PARAGRAPH_FONT_SIZE};
-  }
-  strong {
-    width: 100%;
-    line-height: 1.5;
-    font-weight: ${style.fontWeights.bold};
-    text-align: inherit;
-    font-style: normal;
-    font-size: ${DEFAULT_PARAGRAPH_FONT_SIZE};
-  }
-  a {
-    color: ${style.colours.secondary};
-    text-decoration: none;
-    cursor: pointer;
-    background: none;
-    border: none;
-    padding: 0;
-    font: inherit;
-    outline: inherit;
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-
-  li {
-    margin: 0 0 ${style.spaces.tinier};
-    font-size: ${DEFAULT_PARAGRAPH_FONT_SIZE};
-    font-weight: ${style.fontWeights.light};
-    line-height: 1.5;
-  }
-
-  h2 {
-    font-size: 1.625rem;
-    font-weight: ${style.fontWeights.bold};
-    margin: ${style.spaces.regular} 0;
-  }
-
-  h3 {
-    font-size: ${style.fontSizes.regular};
-    font-weight: ${style.fontWeights.bold};
-    margin: ${style.spaces.big} 0 ${style.spaces.tiny};
-  }
-
-  p > img {
-    margin: auto;
-    display: block;
-  }
 `;
 
 const BlogPostTemplate = ({ data: { markdownRemark: { frontmatter, html } } }) => (
