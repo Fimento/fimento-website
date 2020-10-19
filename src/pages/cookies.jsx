d8c967ecc34ca165ef05b4a9cf07f5551a5afe8e
David Vergnault <david.vergnault@moank.com>
2020-10-19 15:04:53 +0200

    [4127] Create cookie page

---
+++ src/pages/cookies.jsx
@@ -0,0 +1,158 @@
+import React from 'react';
+
+import Section from '../logic/layouts/Section';
+import CatchPhrase from '../logic/layouts/CatchPhrase';
+import Page from '../layout/Page';
+
+import StyledAnchor from '../styled/elements/StyledAnchor';
+import StyledH2 from '../styled/elements/StyledH2';
+import StyledH3 from '../styled/elements/StyledH3';
+import StyledParagraph from '../styled/elements/StyledParagraph';
+import StyledUl from '../styled/elements/StyledUl';
+import StyledLi from '../styled/elements/StyledLi';
+
+import StyledArticle from '../styled/layouts/StyledArticle';
+
+const Cookies = () => (
+  <Page>
+    <Section medium>
+      <CatchPhrase>Cookies</CatchPhrase>
+      <StyledArticle>
+        <StyledH2>Personal data at Fimento</StyledH2>
+        <StyledParagraph>
+          Introduction – What are personal data? In contact with Fimento, in some cases, personal
+          data may be transmitted. “Personal Data” means any kind of information that can directly
+          or indirectly identify a living person. Fimento is personally responsible for the
+          information provided and is available at www.fimento.com. Below is a description of the
+          guidelines that Fimento applies for personal data processing and more. Additional
+          guidelines may apply in specific cases. These guidelines do not limit the rights provided
+          by law or other legally binding provision
+        </StyledParagraph>
+        <StyledH3>Fimento’s general rules on personal data</StyledH3>
+        <StyledParagraph>
+          The general rules mean that Fimento always processes personal data in accordance with
+          applicable privacy laws, respecting personal privacy and all personal data processing
+          rests on a legal basis.
+        </StyledParagraph>
+        <StyledH3>Collection of personal data</StyledH3>
+        <StyledParagraph>
+          Usually, www.fimento.com can be visited without collecting your personal information or
+          similar data. On those types of visits we only collect information that is used for
+          statistical purposes and the visitor remains anonymous. Examples of such information are
+          the duration of the visit, how long the visit lasts and which pages are visited. On the
+          other hand, so-called cookies are used when www.fimento.com are visited, read more about
+          our cookie-policy below. When visiting www.fimento.com can personal information voluntary
+          be given to Fimento via the website. In addition, Fimento may access personal data when,
+          for example, someone sends their information to Fimento by letter, e-mail, telephone or
+          by receiving Fimento data from public records
+        </StyledParagraph>
+        <StyledH3>Purpose</StyledH3>
+        <StyledParagraph>
+          Fimento only collects personal information for certain specific purposes. The general
+          purpose is usually to perform a service (or similar) that the requested person / company
+          has requested. Fimento does not process personal data for purposes that are incompatible
+          with the original purpose. Further information on the purpose for which the personal
+          data is used is in connection with the disclosure of personal data, for example, in
+          connection with the e-mail form or in the terms of the contract.
+        </StyledParagraph>
+        <StyledH3>Consent</StyledH3>
+        <StyledParagraph>
+          Fimento strives to process personal data with the consent of the person concerned. A
+          given consent can always be revoked. If a consent is revoked, Fimento will not process
+          the personal information further or update previously submitted information. For cases
+          where a consent is revoked, treatment is terminated immediately. However, it should be
+          noted that in spite of withdrawn consent, some processing may be necessary to comply with
+          law or agreement.
+        </StyledParagraph>
+        <StyledH3>Transmission of personal data</StyledH3>
+        <StyledParagraph>
+          Personal data may be handed over to partners, for example to complete the request.
+        </StyledParagraph>
+        <StyledH3>Security</StyledH3>
+        <StyledParagraph>
+          Fimento takes technical and organizational measures to protect the personal data being
+          processed so that the data is not lost, destroyed, manipulated or becomes available to
+          unauthorized persons. The measures aim at achieving a level of security that is
+          sufficiently high in view of the technical possibilities available.
+        </StyledParagraph>
+        <StyledH3>Thinning</StyledH3>
+        <StyledParagraph>
+          Fimento does not keep personal data longer than necessary. When personal data is no
+          longer required, they are deleted. This applies, for example, when a question has been
+          answered or when a contractual relationship has been terminated and the
+          parties’ transactions are finalized. In some cases, however, the data may need to be saved
+          even afterwards, for example for accounting purposes.
+        </StyledParagraph>
+        <StyledH3>Right to receive information and corrections</StyledH3>
+        <StyledParagraph>
+          Everyone is entitled to, upon request, find out to what extent information about their
+          own person is treated by Fimento. If such personal data are available at Fimento, the
+          requested person receives the information, upon request, information about which personal
+          data are processed, where they were collected, for which purpose the treatment is being
+          conducted and to whom the information is disclosed. The request for information regarding
+          personal data processing is available at <StyledAnchor href="mailto:info@fimento.com">info@fimento.com</StyledAnchor>. Everyone has the right to
+          request correction, blocking or deletion of personal information that is incorrect or
+          processed in violation of applicable law. Fimento is then obliged to take such action.
+        </StyledParagraph>
+        <StyledH3>Linking</StyledH3>
+        <StyledParagraph>
+          The information on this page applies to Fimento and to the website www.fimento.com, unless
+          otherwise stated. In cases where this website contains links to other websites, the
+          information does not apply to such websites. Fimento also does not respond to the
+          content of such websites.
+        </StyledParagraph>
+        <StyledH3>Changes to personal data processing</StyledH3>
+        <StyledParagraph>
+          If there are changes in how Fimento handles personal information, it will be
+          announced on this page. For example, procedures for treatment may change if the
+          legislation changes.
+        </StyledParagraph>
+        <StyledH3>Contact – Personnel Information</StyledH3>
+        <StyledParagraph>
+          Questions regarding Fimento personal data processing can be addressed to our personal
+          information representative, which can be obtained from <StyledAnchor href="mailto:info@fimento.com">info@fimento.com</StyledAnchor>.
+        </StyledParagraph>
+        <StyledH2>Cookie-policy</StyledH2>
+        <StyledParagraph>
+          A cookie is a small text file that the website you are visiting requests to be saved on
+          your computer. You decide how your computer handles cookies through the settings in your
+          browser. We use both first-party and third-party cookies for different purposes. Some of
+          the cookies placed on your computer are deleted when you leave our site (session cookies)
+          while others remain for up to 2 years on your computer so that the site can recognize you
+          from time to time if you return (permanent cookies) .
+        </StyledParagraph>
+        <StyledParagraph>
+          We do not record information about the visitor’s IP address, therefore the information
+          about you can never be linked to your identity.
+        </StyledParagraph>
+        <StyledH3>We use cookies on our website to:</StyledH3>
+        <StyledUl>
+          <StyledLi>
+            Facilitate the use of the site and allow the pages to load quickly on your computer.
+            (First Party Session Cookie)
+          </StyledLi>
+          <StyledLi>
+            Using Google Analytics analytics tools (third-party session and permanent cookies) to
+            collect statistics about how our visitors move to the site, for example:
+            <StyledUl>
+              <StyledLi>If the visitor has been to the site before</StyledLi>
+              <StyledLi>From where the visitor has clicked into our website</StyledLi>
+              <StyledLi>Which pages are visited on the site</StyledLi>
+              <StyledLi>How long the visit takes</StyledLi>
+              <StyledLi>Which browser is used</StyledLi>
+            </StyledUl>
+          </StyledLi>
+        </StyledUl>
+        <StyledParagraph>
+          If you have any questions about cookies on our website, please contact us
+          at <StyledAnchor href="mailto:info@fimento.com">info@fimento.com</StyledAnchor>. According to the Electronic Communications Act (2003: 389), anyone who
+          visits a website with cookies should have access to information that the website contains
+          cookies and the purpose of the use of cookies. The visitor must also consent to the use
+          of cookies.
+        </StyledParagraph>
+      </StyledArticle>
+    </Section>
+  </Page>
+);
+
+export default Cookies;
